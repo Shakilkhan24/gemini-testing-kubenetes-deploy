519fc88d7130ef088931152d0cd74627ab7b95a1
SHAKIL <sk2448868@gmail.com>
2025-01-20 15:21:30 +0600

    added multiple files , explored certain things

--- helm.sh
+++ helm.sh
@@ -1,1 +1,6 @@
-# Write usefull helm commands , would be handy if forgot.
+# Update Chart.yaml (helm addons charts) and Install the Addons mentioned there with specific versions
+helm dependency update path[Chart.yaml]
+
+# Go inside the folder Chart.yaml is in. Then helm dependency update .
+# It keeps only addons is included in the Chart.yaml or not
+# Since it is update operation, also delete all previous versions 
