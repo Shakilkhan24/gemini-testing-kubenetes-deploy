7cfb5c6d7d828e54055f9ad8d2a998e37b03b448
SHAKIL <sk2448868@gmail.com>
2025-01-20 19:59:17 +0600

    Installing a helm chart with custom values for the Chart.yaml resources like grafana , loki etc

--- helm.sh
+++ helm.sh
@@ -7,3 +7,31 @@ helm dependency update path[Chart.yaml]
 
 # ... for installation helm chart ...
 helm install release1 ./project_manager
+
+
+
+# We can install or upgrade an entire helm chart with our custom values for the resources of 
+# .......... Chart.yaml ...........
+# helm install or update 
+
+
+# helm repo add grafana https://grafana.github.io/helm-charts
+# helm show values grafana/grafana > grafana-values.yaml
+
+# ----------------------------------------------------------- #
+helm install my-releasex ./project_manager \
+  -f values.yaml \
+  -f ./project_manager/Values/grafana-values.yaml \
+  -f ./project_manager/Values/prometheus-values.yaml \
+  -f ./project_manager/Values/ingress-nginx-values.yaml 
+
+# Find helm charts in here [https://artifacthub.io/packages/search?kind=0]
+
+# ---- If you want, you can directly add some customization , but through values.yaml is more flexible...
+helm install jaeger jaegertracing/jaeger \
+  --set provisionDataStore.cassandra=false \
+  --set storage.type=elasticsearch \
+  --set storage.elasticsearch.host=<HOST> \
+  --set storage.elasticsearch.port=<PORT> \
+  --set storage.elasticsearch.user=<USER> \
+  --set storage.elasticsearch.password=<password>
